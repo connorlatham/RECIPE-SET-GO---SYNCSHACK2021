075bc3d4cd23cd3b4c932594cc96795fc8f0d605
Connor Latham <calatham6@gmail.com>
2021-08-28 16:59:42 +1000

    Added some very basic stuff

--- server.py
+++ server.py
@@ -2,7 +2,17 @@
 This will be the file that runs our web server for this project
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import csv
+
+f = open("recipes.csv", "r")
+
+lines = csv.reader(f)
+
+recipes = []
+
+for line in lines:
+    recipes.append(line)
 
 app = Flask(__name__)
 
@@ -11,8 +21,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#def home():
-#    return "Hello, World!"
+@app.route('/', methods=['POST'])
+def my_form_post():
+    words = request.form['INPUT'].upper().split(" ")
+    
+    counter = 0
+    for recipe in recipes:
+        wordcheck = True
+        for word in words:
+            if word not in recipe[1]:
+                wordcheck = False
+        if wordcheck:
+            counter += 1
+    return render_template('index.html', food = words, number = counter)
     
 if __name__ == "__main__":
     app.run(debug=True)
